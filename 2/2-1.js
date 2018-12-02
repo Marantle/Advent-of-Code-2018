ea03522cce1c71d6304c63bb35a7a82f16fc49df
Marantle <leri6@hotmail.com>
2018-12-02 22:05:05 +0200

    formatting

--- 2/2-1.js
+++ 2/2-1.js
@@ -1,14 +1,14 @@
 const hasDupes = (o, m) => {
-    return Object.values(o).filter(v => v === m).length > 0
+  return Object.values(o).filter(v => v === m).length > 0
 }
 const texts = document.querySelector('body > pre:nth-child(1)').textContent.split("\n").filter(t => t.length > 0);
 const results = []
 for (let text of texts) {
-    const dupes = {}
-    for (let c of text) {
-        dupes[c] ? dupes[c] = dupes[c] + 1: dupes[c] = 1
-    }    
-    results.push(dupes)
+  const dupes = {}
+  for (let c of text) {
+    dupes[c] ? dupes[c] = dupes[c] + 1 : dupes[c] = 1
+  }
+  results.push(dupes)
 }
 const doubleBoxes = results.filter(c => hasDupes(c, 2))
 const tripleBoxes = results.filter(c => hasDupes(c, 3))
