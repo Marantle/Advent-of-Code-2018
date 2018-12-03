92697740bdd09a40197685562937289d4a914c3b
Marko Leppänen <leri6@hotmail.com>
2018-12-03 18:08:23 +0200

    Update 1-1.js

--- 1/1-1.js
+++ 1/1-1.js
@@ -1,11 +1,8 @@
-const element = document.querySelector('body > pre:nth-child(1)');
-const text = element.textContent;
-const texts = text.split("\n");
+const texts = document.querySelector('body > pre:nth-child(1)').textContent.split("\n");
 let result = 0;
 for (let t of texts) {
   if (!isNaN(t) && t.length > 1) {
-    console.log('val', parseInt(t))
     result += parseInt(t)
   }
 }
-console.log(result)
+console.log('freq: ', result)
